12a68203ebf678560659dbcef12b8b6813a9cc21
Heitor Zimmer <heitorzs@hotmail.com>
2023-09-14 20:04:29 -0300

    add click function

--- src/Components/TicTacToe.jsx
+++ src/Components/TicTacToe.jsx
@@ -1,15 +1,36 @@
 import React, { useState } from 'react'
 import Board from './Board'
 
+const playerX = 'X';
+const playerO = 'O';
+
 export default function TicTacToe() {
     const [tiles, setTiles] = useState(Array(9).fill(null));
+    const [playerTurn, setPlayerTurn] = useState(playerX)
 
+    const handleTileClick = (index) => {
+        if (tiles[index] !== null) { //checking if tiles has content
+            return
+        }
+        const newTiles = [...tiles];
+        newTiles[index] = playerTurn;
+        setTiles(newTiles);
+        if (playerTurn === playerX) { //changing player turn 
+            setPlayerTurn(playerO)
+        } else {
+            setPlayerTurn(playerX)
+        }
+    }
     return (
         <div>
             <h1>Jogo da Velha</h1>
 
             <div>
-                <Board tiles={tiles} />
+                <Board
+                    tiles={tiles}
+                    onTileClick={handleTileClick}
+                    playerTurn={playerTurn}
+                />
             </div>
 
         </div>
--- src/Components/Tile.jsx
+++ src/Components/Tile.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
-export default function Tile() {
-  return (
-    <div className='tile'>X</div>
-  )
+export default function Tile({ value, onClick, className, playerTurn}) {
+    let hoverClass = null;
+    if(value == null && playerTurn != null){
+        hoverClass = `${playerTurn.toLowerCase()}-hover`
+    }
+    return (
+        <div onClick={onClick} className={`tile ${className} ${hoverClass}`}>{value}</div>
+    )
 }
+ 
